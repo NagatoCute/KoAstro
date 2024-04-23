e2a28c601f8027c7b459aec108d61fb2772053e6
NagatoCute <1659103717@qq.com>
2024-04-23 22:48:05 +0800

    en

--- src/consts.ts
+++ src/consts.ts
@@ -1,17 +1,28 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Koibumi Blog';
+export const SITE_TITLE = 'Lux\'s Blog';
 
 export const SITE_DESCRIPTION = 'A blog about SolidJS, Astro, and koibumi design.';
 
-export const FAVICON_SRC = '/favicon.svg';
+export const FAVICON_SRC = '/favicon.ico';
 
 export const AVATAR_SRC = '/avatar.avif';
 
-export const BIO = " Welcome to my website! I write about programming, technology, and other things that interest me."
 
-export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
+export const BIO = " 欢迎来到我的网站\，现在还没有做任何修改\。"
 
-export const COPYRIGHT_NAME = 'Koibumi Design';
+// export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
+export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass';
 
+// export const COPYRIGHT_NAME = 'Koibumi Design';
+export const COPYRIGHT_NAME = `
+Lux-Aeris
+`;
+// SITE_TITLE	站点的标题。
+// SITE_DESCRIPTION	站点的描述。
+// FAVICON_SRC	favicon的路径。
+// AVATAR_SRC	头像的路径。
+// BIO	作者的简介。
+// GLOBAL_STYLE	站点的全局样式。您可以从quartz、glass和lightGlass中选择一种。
+// COPYRIGHT_NAME	版权名称。
---
+++ src/content/blog/recode.md
@@ -0,0 +1,40 @@
+---
+title: '修改记录'
+description: '这是本站点的修改记录。'
+pubDate: 'Apr 23 2024'
+heroImage: '/117092881_p0.webp'
+pinned: true
+---
+> 个人站点修改记录 
+
+## 原作品信息
+
+- **原作者**：haruki-nikaidou
+- **原作品标题**：恋文 Astro Blog
+- **原作品链接**：https://github.com/koibumi-design/astro-blog
+- **原作品授权协议**：[CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/) — 知识共享署名-非商业性使用 4.0 国际许可协议
+
+## 修改概述
+
+本作品基于原作者 haruki-nikaidou 发布的恋文 Astro Blog（遵循 CC BY-NC 4.0 许可协议）进行了以下修改：
+
+### 主要改动点
+
+暂时没有
+
+<!-- 1. [详细描述第一项主要修改内容及其目的或改进之处]
+2. [详细描述第二项主要修改内容及其目的或改进之处]
+3. ...
+4. ... -->
+
+### 辅助调整与优化
+
+- 改变了页脚的授权协议
+
+<!-- - [列出次要修改或优化，如样式调整、代码重构、文档更新等] -->
+
+## 使用注意事项
+
+暂无
+
+---
