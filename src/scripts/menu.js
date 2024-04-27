f37adef47e1b506172e5b10c067781e4abc429e3
NagatoCute <1659103717@qq.com>
2024-04-27 20:33:48 +0800

    微调

---
+++ src/scripts/menu.js
@@ -0,0 +1,6 @@
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//     document.querySelector('.internal-links').classList.toggle('expanded');
+// });
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.querySelector('.internal-links').classList.toggle('expanded');
+});
